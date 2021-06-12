ad4f7ea48eccebaad019650d648aee5fe045900c
CNDvn <banhduchieucode@gmail.com>
2021-06-12 22:58:59 +0700

    Dashbroad User done

--- src/containers/AdminTemplate/AdminService/AdminService.jsx
+++ src/containers/AdminTemplate/AdminService/AdminService.jsx
@@ -1,4 +1,9 @@
 import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
 const config = (method, url, token, data) => {
   return {
     method: method,
@@ -13,6 +18,22 @@ const config = (method, url, token, data) => {
 };
 
 class AdminService {
+  notification = (notifi,type)=>{
+    const tmp =  {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      }
+
+    if(type==='success')
+      toast.success(notifi,tmp);
+    if(type==='error')
+      toast.error(notifi,tmp);
+  }
   getListRoleUser = () => {
     return axios(config("get", "https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/LayDanhSachLoaiNguoiDung"));
   };
@@ -22,11 +43,14 @@ class AdminService {
   getAllMovie = () => {
     return axios(config("get", "https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01"));
   };
-  deleteAUser = ()=>{
-
+  deleteAUser = (token, username)=>{
+    return axios(config("delete", `https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/XoaNguoiDung?taiKhoan=${username}`,token,""));
   };
   editAUser = (token, data)=>{
     return axios(config("put","https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/CapNhatThongTinNguoiDung",token, data))
+  }
+  AddAUser = (token, data)=>{
+    return axios(config("post","https://movie0706.cybersoft.edu.vn/api/QuanLyNguoiDung/ThemNguoiDung",token, data))
   }
 }
 
--- src/containers/AdminTemplate/DashboardPage/DashboardUser/index.jsx
+++ src/containers/AdminTemplate/DashboardPage/DashboardUser/index.jsx
@@ -2,13 +2,48 @@ import React from "react";
 import MaterialTable from "material-table";
 import adminService from "../../AdminService/AdminService";
 import PopupUser from "containers/AdminTemplate/component/PopupUser";
+import { ToastContainer, toast } from 'react-toastify';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
 
 export default function DasboardUser() {
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const [userDelete, setUserDelete] = React.useState();
+  
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleCancel = () => {
+    setUserDelete("");
+    setOpen(false);
+  };
+
+  const handleDelete = () => {
+    adminService.deleteAUser(token,userDelete.taiKhoan).then((res)=>{
+      adminService.notification(res.data, "success");
+      setFlag(!flag)
+    }).catch((err)=>{
+      adminService.notification(err.response.data, "error");
+      console.log({...err});
+    })
+    setOpen(false);
+  };
+
   const [allUser, setAllUser] = React.useState([]);
   //status 0 = not thing, 1 = display Edit popupUser, 2 = display Add User popupUser
   const [statusPopup, setstatusPopup] = React.useState(0);
   const [userEdit, setuserEdit] = React.useState();
-  const [flag, setFlag] = React.useState(false)
+  const [flag, setFlag] = React.useState(false);
   React.useEffect(() => {
     try {
       const callApi = async () => {
@@ -25,30 +60,89 @@ export default function DasboardUser() {
     setstatusPopup(0);
     setFlag(!flag);
   };
-  const tokenTest =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiaGlldTEyM2FiYyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IktoYWNoSGFuZyIsIm5iZiI6MTYyMjY4NzU3MSwiZXhwIjoxNjIyNjkxMTcxfQ.VH3N1iXRbbx7ZsQ3GH0zG4sOVL6GHuFxNh53DVLR3ms";
-  const handleEditUser = (data) => {
-    try {
-      const callApi = async () => {
-        await adminService.editAUser(tokenTest,data);
-      };
-      callApi();
-    } catch (err) {
+
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYXN1a2ExOTk3IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiUXVhblRyaSIsIm5iZiI6MTYyMzUxMTQ4NiwiZXhwIjoxNjIzNTE1MDg2fQ.A9ZpE-ZQ6NHTU5xq1TfS7s7s-2Nmu9Ur-Hfv1IWCyVI";
+  // JSON.parse(localStorage.getItem("UserLogin")).accessToken;
+  const handleEditUser = (data,flagShowNoti) => {
+    adminService.editAUser(token, data).then((res)=>{
+        adminService.notification("Success", "success");
+    }).catch((err)=>{
+      if(flagShowNoti)
+        adminService.notification(err.response.data, "error");
       console.log({...err});
-    }
+    });
   };
+
+  const handleAddUser = (data,flagShowNoti) => {
+    adminService.AddAUser(token, data).then((res)=>{
+      adminService.notification("Success", "success");
+    }).catch((err)=>{
+      if(flagShowNoti)
+        adminService.notification(err.response.data, "error");
+      console.log({...err});
+    });
+  };
+      
   return (
     <>
+    <div>
+      <Dialog
+        fullScreen={fullScreen}
+        open={open}
+        onClose={handleCancel}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">{"Delete User"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Do you sure want to delete user: {userDelete && userDelete.hoTen}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleCancel} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleDelete} color="primary" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      
       {statusPopup === 1 && (
         <PopupUser
           title="Edit"
           user={userEdit}
           handleClosePopup={handleClosePopup}
-          handleUser = {handleEditUser}
+          handleUser={handleEditUser}
         />
       )}
       {statusPopup === 2 && (
-        <PopupUser title="Add User" handleClosePopup={handleClosePopup} />
+        <PopupUser
+          user={{
+            email:"",
+            hoTen:"",
+            maLoaiNguoiDung:"",
+            matKhau:"",
+            soDt:"",
+            taiKhoan:""
+          }}
+          title="Add User"
+          handleClosePopup={handleClosePopup}
+          handleUser={handleAddUser}
+        />
       )}
       <div style={{ maxWidth: "100%" }}>
         <MaterialTable
@@ -84,13 +178,16 @@ export default function DasboardUser() {
               onClick: (event, rowData) => {
                 setstatusPopup(1);
                 setuserEdit(rowData);
+                console.log(rowData);
               },
             },
             {
               icon: "delete",
               tooltip: "Delete User",
-              onClick: (event, rowData) =>
-                window.confirm("You want to delete " + rowData.taiKhoan),
+              onClick: (event, rowData) =>{
+                setUserDelete(rowData)
+                handleClickOpen()
+              }
             },
           ]}
         />
