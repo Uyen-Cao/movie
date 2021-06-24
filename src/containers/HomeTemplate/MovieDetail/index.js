adb284f140e58a5c91e038f70a175ffe66840572
Uyen-Cao <caophuonguyen132002@gmail.com>
2021-06-24 21:52:39 +0700

    Booking Page

--- src/containers/HomeTemplate/MovieDetail/index.js
+++ src/containers/HomeTemplate/MovieDetail/index.js
@@ -1,12 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./style/index.css";
 import { Button } from "@material-ui/core";
 import { fetchDetailMovie } from "./modules/action";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "components/Loader";
-import ShowtimeTable from "components/ShowtimeTable";
+import Popup from "components/Popup";
 
 export default function MovieDetailPage(props) {
+  const [button, setButton] = useState(false);
   const dispatch = useDispatch();
   const data = useSelector((state) => state.detailMovieReducer.data);
   const loading = useSelector((state) => state.detailMovieReducer.loading);
@@ -25,7 +26,7 @@ export default function MovieDetailPage(props) {
       <div className="movie-menubar"></div>
       <div className="movie-carousel">
         <div className="container movie-details">
-          <div className="row text-center">
+          <div className="row wrapper text-center">
             <div className="movie-content col-8 text-left text-light">
               <Button className="button-limitage" variant="contained">
                 C18
@@ -45,16 +46,27 @@ export default function MovieDetailPage(props) {
                 <Button variant="contained" color="secondary">
                   Đặt vé
                 </Button>
-                <Button className="mx-2 my-3" variant="contained">
+                <Button
+                  onClick={() => setButton(true)}
+                  className="mx-2 my-3"
+                  variant="contained"
+                >
                   Trailer
                 </Button>
               </div>
             </div>
-            <div className="col-4">
-              <img className="movie-poster" src={data && data.hinhAnh} />
+            <div className="col-4 movie-poster">
+              <img src={data && data.hinhAnh} />
             </div>
           </div>
         </div>
+      </div>
+      <div>
+        <Popup
+          movieTrailer={data && data.trailer}
+          trigger={button}
+          setTrigger={setButton}
+        />
       </div>
     </div>
   );
